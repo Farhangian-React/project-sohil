94fbe44b318cbfe9cd16fe8333d02730fd2f252a
farhangin-elham <elhamfarhangian6@gmail.com>
2025-05-24 01:21:20 +0330

    set63

--- src/Product/Refrigerator/RefrideratorTopFreezer/CardRefrideratorTopFreezer.js
+++ src/Product/Refrigerator/RefrideratorTopFreezer/CardRefrideratorTopFreezer.js
@@ -1,89 +1,13 @@
 import React, {useContext,useState,useEffect} from 'react';
-import {CartContext} from '../../../Shared/Cart-Context';
 import {CardData} from '../../../Shared/Cart-Context';
 import {CardDataShow} from '../../../Shared/Cart-Context';
 import ReactPaginate from 'react-paginate';
 import { NavLink } from 'react-router-dom';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import FilteringTopFreezer from './FilteringTopFreezer';
-import Rating from '@mui/material/Rating';
+import FilteringRefrigerators from "../FilteringRefrigerators";
+import Cards from '../../Cards';
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
 function Tems({ currentItems,load }) {
-      const [cartItems,setCartItems]=useContext(CartContext);
-      const[datacomment,setDatacomment]=useState([]);
-      const convertToEnglish=(str)=> {
-       
-        let englishNumber =str
-        .replace(/۰/g, '0')
-        .replace(/۱/g, '1')
-        .replace(/۲/g, '2')
-        .replace(/۳/g, '3')
-        .replace(/۴/g, '4')
-        .replace(/۵/g, '5')
-        .replace(/۶/g, '6')
-        .replace(/۷/g, '7')
-        .replace(/۸/g, '8')
-        .replace(/۹/g, '9');
-    
-     return(englishNumber);
-      }
-      const convertToPersian=(str)=> {
-        let persianNumber =str
-        .replace(/0/g, '۰')
-        .replace(/1/g, '۱')
-        .replace(/2/g, '۲')
-        .replace(/3/g, '۳')
-        .replace(/4/g, '۴')
-        .replace(/5/g, '۵')
-        .replace(/6/g, '۶')
-        .replace(/7/g, '۷')
-        .replace(/8/g, '۸')
-        .replace(/9/g, '۹');
-    return(persianNumber);
-      }
-      const addCommas=(nStr)=>{
-      
-        if(nStr.length === 7){
-          return  nStr.substring(0,1) + "," + nStr.substring(1, 4) + "," + nStr.substring(4, 7);
-        }
-        if(nStr.length === 8){
-          return  nStr.substring(0, 2) + "," + nStr.substring(2, 5) + "," + nStr.substring(5, 8);
-        }
-        if(nStr.length === 9){
-          return  nStr.substring(0, 3) + "," + nStr.substring(3, 6) + "," + nStr.substring(6, 9);
-      }
-      }
-    const getData=()=>{
-      fetch("https://serverjson-project.onrender.com/Allcomments")
-      .then((res)=>
-         res.json())
-         .then((data)=>{
-        setDatacomment(data);
-          console.log(cartItems);
-        
-        }
-         )
-      .catch(err=>console.log(err));
-    }
-    useEffect(()=>{
-    
-      getData();
-      
-    },[])
-        const sumscore=(item)=>{
-          let total=0;
-          let aver=0;
-       datacomment.map(i=>{
-        if(i.idcomment === item.id){
-          aver ++;
-          total += i.score; 
-        }
-        });
-         return total /aver;
-       }
    return (
     <> 
      <div className='flex flex-col bg-zinc-100 justify-center m-auto'>
@@ -100,7 +24,7 @@ function Tems({ currentItems,load }) {
     <div className='flex flex-col md:flex-row justify-start w-[100vw] bg-zinc-100 m-auto' >
       <div className='flex w-[90vw] md:w-[30vw] static md:sticky md:top-[80px] self-center md:self-start md:h-[70vh] '>
 
- <FilteringTopFreezer/></div>
+ <FilteringRefrigerators/></div>
  
  <div className='flex justify-center flex-wrap mx-auto bg-zinc-100 '>
   {
@@ -115,74 +39,7 @@ function Tems({ currentItems,load }) {
   currentItems &&
     currentItems.map((item) => (
       <NavLink to={'/cartbuyrefrigerator'}  className={"linkss"}>    
- 
- <Card className='cards' sx={{width:{xs:"250px",md:"300px"},height:"460px",mx:1,my:"1em",px:1 ,pt:3 }} key={item.id}>
-
-        <CardMedia
-    component="img"
-    onClick={()=>{
-      setCartItems([item]);
-     }}
-   image={item.img}
-    alt=""
-sx={{width:"250px",m:"auto",height:"200px"}}
-  />
-    <CardContent sx={{border:"none",height:'55px',direction:"rtl"}}>
-    <p className='text-center text-sm font-bold pb-1'>
-      {item.title1}
-    </p>
-  </CardContent>
-    <CardContent sx={{display:"flex",flexDirection:"row",justifyContent:"space-around",mt:1.5,
-    alignSelf:"center",alignItems:"center",
-    border:"none",height:'40px',direction:"rtl",py:0.5,my:0}}>
-    <p className='text-center text-xs text-gray-700'>
-      {item.title2}
-      </p>
-      <Rating
-         sx={{color:"#f5cd00",fontSize:"1em",pr:5,alignItems:"center"}}
-        name="simple-controlled"
-        value={sumscore(item)}
-      
-      />
-    
-  </CardContent>
-  
-  <CardContent sx={{height:'70px',direction:"rtl",display:"flex",justifyContent:"center",flexDirection:"column",borderBottom:'1px solid #d4d4d4',borderTop:'1px solid #d4d4d4'}} >
-
- { item.off !== "" ?
-      
-          <div  dir="rtl" className='flex justify-between items-baseline'>
-          <div   className='flex justify-center text-center rounded-md text-yellow-500 bg-gray-900 h-[20px] w-[35px]
-           hover:bg-yellow-500 hover:text-gray-900'>
-        <p className='text-xs text-center py-0.5' >  {convertToPersian(item.off)}%</p>
-        
-        </div> 
-          <div className='flex flex-col' >
-        <p className='text-sm text-yellow-500 text-center line-through decoration-gray-600 ' >{item.price}</p>
-         <p   className='text-gray-600 text-left text-lg font-bold' >
-       
-          {addCommas(convertToPersian((convertToEnglish(item.pricenum)-(convertToEnglish(item.pricenum)*item.off / 100))+''))} تومان 
-         </p>
- </div>
- </div>
-
-:<p className='text-lg text-center text-gray-600 font-bold '>{item.price}</p>
- }
-  </CardContent>
-  <CardContent sx={{display:'flex',justifyContent:'center',height: "30px"}}>
- <NavLink style={{ textDecoration: "none" }} to={"/cartbuyrefrigerator" }>
-                     <button    onClick={()=>{
-               setCartItems([item]);
-              }} className='bg-gradient-to-r from-yellow-500 via-yellow-900 to-yellow-500 
-                     rounded-tr-md rounded-bl-md rounded-tl-2xl rounded-br-2xl my-1 mx-8 px-10 py-1 text-white text-md font-sans
-                      hover:bg-gradient-to-r hover:from-gray-500
-                     hover:via-yellow-500 hover:to-gray-500'
-                      > 
-                     خرید</button></NavLink>
-    
-  </CardContent>
-</Card>
-
+   <Cards curr={item}/>
 </NavLink>
       
     ))}
--- src/Product/WashingMachine/SlideSimilarWash.js
+++ src/Product/WashingMachine/SlideSimilarWash.js
@@ -1,11 +1,8 @@
 
 
-     import * as React from 'react';
+     import React from 'react';
      import  {useContext} from 'react';
      import { CartContext} from '../../Shared/Cart-Context';
-     import { Typography,Button } from '@mui/material';
-     import Box from '@mui/material/Box';
-     import Divider from '@mui/material/Divider';
      import { NavLink } from 'react-router-dom';
      import "react-toastify/dist/ReactToastify.css";
      import '../CartBuy.css';
@@ -14,11 +11,11 @@
      import {CardDataShow} from '../../Shared/Cart-Context';
      import { Fade } from 'react-slideshow-image';
      import { Slide } from 'react-slideshow-image';
+     import Cards from '../Cards';
      
      
      
      export default function SlideSimilarWash()  {
-         const [cartItems,setCartItems]=useContext(CartContext);
          const [data,setData]=useContext(CardData);
          const [datashow,setDatashow]=useContext(CardDataShow);
         const getData=()=>{
@@ -36,57 +33,6 @@
         useEffect(()=>{
          getData();
        },[])
-     
-       const convertToEnglish=(str)=> {
-        
-         let englishNumber =str
-         .replace(/۰/g, '0')
-         .replace(/۱/g, '1')
-         .replace(/۲/g, '2')
-         .replace(/۳/g, '3')
-         .replace(/۴/g, '4')
-         .replace(/۵/g, '5')
-         .replace(/۶/g, '6')
-         .replace(/۷/g, '7')
-         .replace(/۸/g, '8')
-         .replace(/۹/g, '9');
-     
-      return(englishNumber);
-       }
-       const convertToPersian=(str)=> {
-        
-         let persianNumber =str
-         .replace(/0/g, '۰')
-         .replace(/1/g, '۱')
-         .replace(/2/g, '۲')
-         .replace(/3/g, '۳')
-         .replace(/4/g, '۴')
-         .replace(/5/g, '۵')
-         .replace(/6/g, '۶')
-         .replace(/7/g, '۷')
-         .replace(/8/g, '۸')
-         .replace(/9/g, '۹');
-     return(persianNumber);
-       }
-        useEffect(()=>{
-         getData();
-       
-     
-        
-       
-       },[])
-       const addCommas=(nStr)=>{
-       
-         if(nStr.length === 7){
-           return  nStr.substring(0,1) + "," + nStr.substring(1, 4) + "," + nStr.substring(4, 7);
-         }
-         if(nStr.length === 8){
-           return  nStr.substring(0, 2) + "," + nStr.substring(2, 5) + "," + nStr.substring(5, 8);
-         }
-         if(nStr.length === 9){
-           return  nStr.substring(0, 3) + "," + nStr.substring(3, 6) + "," + nStr.substring(6, 9);
-       }
-       }
          return(
              <><div dir='rtl' className=' flex flex-col md:hidden bg-white my-2 mx-auto w-[97%] rounded-sm '>
                      <div    className='bordershadow flex flex-col justify-center w-[200px] text-start m-10 '  >
@@ -96,81 +42,12 @@
                      </div>
                      <Fade>
                      {datashow.map((item) => (
-                   
                    <NavLink to={'/cartbuywashingmachine'}  className={"linkss"}>
-             
-             <div
-                 onClick={()=>{
-                  setCartItems([item]);
-                 }}
-                 key={item.id} className='flex justify-center items-center mx-auto px-0 backdrop-blur-lg rounded-3xl '>
-                   <div  className='bg-white  w-[400px] h-[620px]  rounded-lg shadow-xl
-               mx-3 my-3
-                   ' >
-             
-                      
-             <Box    
-                       sx={{display:"flex", height:"420px",borderRadius:"10px",justifyContent:'center',
-                      backgroundImage: `url(${item.img})`,
-                      backgroundPosition: 'center',
-                      backgroundSize:'cover',
-                      backgroundRepeat: 'no-repeat',':hover':{  backgroundImage: `url(${item.imga1})`,
-                      backgroundPosition: 'center',
-                      backgroundSize:'cover',scale:"1.1"}}}
-                     
-                       
-                       >     
-                        </Box>    
-                        <div dir="rtl" className=' pt-5 px-5 '>
-                        <div className='flex justify-center h-[50px]'>
-             <p className='text-sm text-center text-gray-800 font-bold'> {item.title1}</p> 
-                    </div>
-             
-             
-             <div className='flex flex-col justify-center  p-1 '>
-               <div className='flex flex-col h-[70px] w-full border-b-2 border-t-2 py-0.5 self-center justify-center border-gray-200'>
-          { item.off !== "" ?
-           
-               <div  dir="rtl" className='flex justify-between items-baseline'>
-               <div   className='flex justify-center text-center rounded-md text-yellow-500 bg-gray-900 h-[20px] w-[35px]
-                hover:bg-yellow-500 hover:text-gray-900'>
-             <p className='text-xs text-center py-0.5' >  {convertToPersian(item.off)}%</p>
-             
-             </div> 
-               <div className='flex flex-col' >
-             <p className='text-sm text-yellow-500 text-center line-through decoration-gray-600 ' >{item.price}</p>
-              <p   className='text-gray-600 text-left text-lg font-bold' >
-            
-               {addCommas(convertToPersian((convertToEnglish(item.pricenum)-(convertToEnglish(item.pricenum)*item.off / 100))+''))} تومان 
-              </p>
-      </div>
-      </div>
-     
-     :<p className='text-lg text-center text-gray-600 font-bold '>{item.price}</p>
-      } </div>
-        <div className='flex justify-center h-auto mt-2  '>
-             <NavLink style={{ textDecoration: "none" }} to={"/cartbuywashingmachine" }>
-                                 <button    onClick={()=>{
-                           setCartItems([item]);
-                          }} className=' my-1 mx-8 px-10 py-1 text-md font-sans bg-gradient-to-r from-yellow-500 via-gray-500 to-yellow-500 
-                                        rounded-tr-md rounded-bl-md rounded-tl-2xl rounded-br-2xl  text-white
-                                         hover:bg-gradient-to-r hover:from-gray-500
-                                        hover:via-yellow-800 hover:to-gray-500'
-                                  >
-                                 خرید</button></NavLink>
-                                 </div>
-             </div>
-                         </div>
-                    </div>
-                  </div>
+               <Cards curr={item}/>
              </NavLink>
-                   
                  ))}
-                
                      </Fade>
                      </div>
-             
-                    
                      <div dir='rtl' className='hidden md:flex  md:flex-col lg:hidden bg-white my-2 mx-auto w-[97%] rounded-sm '>
                      <div    className='bordershadow flex flex-col justify-center w-[200px] text-start m-10 '  >
                      <p className='text-2xl text-center font-bold text-gray-700 py-3' >
@@ -179,74 +56,8 @@
                      </div>
                      <Slide slidesToScroll={1} slidesToShow={3} indicators={false} >
                      {datashow.map((item) => (
-                   
                    <NavLink to={'/cartbuywashingmachine'}  className={"linkss"}>
-             
-             <div
-                 onClick={()=>{
-                  setCartItems([item]);
-                 }}
-                 key={item.id} className='flex justify-center items-center mx-auto px-0 backdrop-blur-lg rounded-3xl '>
-                   <div  className='bg-white  w-[320px] h-[500px]  rounded-lg shadow-xl
-               mx-3 my-3
-                   ' >
-             
-                      
-             <Box    
-                       sx={{display:"flex", height:"280px",borderRadius:"10px",justifyContent:'center',
-                      backgroundImage: `url(${item.img})`,
-                      backgroundPosition: 'center',
-                      backgroundSize:'cover',
-                      backgroundRepeat: 'no-repeat',':hover':{  backgroundImage: `url(${item.imga1})`,
-                      backgroundPosition: 'center',
-                      backgroundSize:'cover',scale:"1.1"}}}
-                     
-                       
-                       >     
-                        </Box>    
-                        <div dir="rtl" className=' pt-5 px-5 '>
-                        <div className='flex justify-center h-[50px]'>
-             <p className='text-sm text-center text-gray-800 font-bold'> {item.title1}</p> 
-                    </div>
-             
-             
-             <div className='flex flex-col justify-center  p-1 '>
-                <div className='flex flex-col h-[70px] w-full border-b-2 border-t-2 py-0.5 self-center justify-center border-gray-200'>
-            { item.off !== "" ?
-           
-               <div  dir="rtl" className='flex justify-between items-baseline'>
-               <div   className='flex justify-center text-center rounded-md text-yellow-500 bg-gray-900 h-[20px] w-[35px]
-                hover:bg-yellow-500 hover:text-gray-900'>
-             <p className='text-xs text-center py-0.5' >  {convertToPersian(item.off)}%</p>
-             
-             </div> 
-               <div className='flex flex-col' >
-             <p className='text-sm text-yellow-500 text-center line-through decoration-gray-600 ' >{item.price}</p>
-              <p   className='text-gray-600 text-left text-lg font-bold' >
-            
-               {addCommas(convertToPersian((convertToEnglish(item.pricenum)-(convertToEnglish(item.pricenum)*item.off / 100))+''))} تومان 
-              </p>
-      </div>
-      </div>
-     
-     :<p className='text-lg text-center text-gray-600 font-bold '>{item.price}</p>
-      }
-      </div>
-             <div className='flex justify-center h-auto mt-2  '>
-             <NavLink style={{ textDecoration: "none" }} to={"/cartbuywashingmachine" }>
-                                 <button    onClick={()=>{
-                           setCartItems([item]);
-                          }} className=' my-1 mx-8 px-10 py-1 text-md font-sans bg-gradient-to-r from-yellow-500 via-gray-500 to-yellow-500 
-                                        rounded-tr-md rounded-bl-md rounded-tl-2xl rounded-br-2xl  text-white
-                                         hover:bg-gradient-to-r hover:from-gray-500
-                                        hover:via-yellow-800 hover:to-gray-500'
-                                  > 
-                                 خرید</button></NavLink>
-                                 </div>
-             </div>
-                         </div>
-                    </div>
-                  </div>
+               <Cards curr={item}/>
              </NavLink>
                    
                  ))}
@@ -263,72 +74,7 @@
                      {datashow.map((item) => (
                    
                    <NavLink to={'/cartbuywashingmachine'}  className={"linkss"}>
-             
-             <div
-                 onClick={()=>{
-                  setCartItems([item]);
-                 }}
-                 key={item.id} className='flex justify-center items-center mx-auto px-0 backdrop-blur-lg rounded-3xl '>
-                   <div  className='bg-white  w-[320px] h-[500px]  rounded-lg shadow-xl
-               mx-3 my-3
-                   ' >
-             
-                      
-             <Box    
-                       sx={{display:"flex", height:"280px",borderRadius:"10px",justifyContent:'center',
-                      backgroundImage: `url(${item.img})`,
-                      backgroundPosition: 'center',
-                      backgroundSize:'cover',
-                      backgroundRepeat: 'no-repeat',':hover':{  backgroundImage: `url(${item.imga1})`,
-                      backgroundPosition: 'center',
-                      backgroundSize:'cover',scale:"1.1"}}}
-                     
-                       
-                       >     
-                        </Box>    
-                        <div dir="rtl" className=' pt-5 px-5 '>
-                        <div className='flex justify-center h-[50px]'>
-             <p className='text-sm text-center text-gray-800 font-bold'> {item.title1}</p> 
-                    </div>
-             
-             
-             <div className='flex flex-col justify-center  p-1 '>
-                <div className='flex flex-col h-[70px] w-full border-b-2 border-t-2 py-0.5 self-center justify-center border-gray-200'>
-           { item.off !== "" ?
-           
-               <div  dir="rtl" className='flex justify-between items-baseline'>
-               <div   className='flex justify-center text-center rounded-md text-yellow-500 bg-gray-900 h-[20px] w-[35px]
-                hover:bg-yellow-500 hover:text-gray-900'>
-             <p className='text-xs text-center py-0.5' >  {convertToPersian(item.off)}%</p>
-             
-             </div> 
-               <div className='flex flex-col' >
-             <p className='text-sm text-yellow-500 text-center line-through decoration-gray-600 ' >{item.price}</p>
-              <p   className='text-gray-600 text-left text-lg font-bold' >
-            
-               {addCommas(convertToPersian((convertToEnglish(item.pricenum)-(convertToEnglish(item.pricenum)*item.off / 100))+''))} تومان 
-              </p>
-      </div>
-      </div>
-     
-     :<p className='text-lg text-center text-gray-600 font-bold '>{item.price}</p>
-      }
-      </div>
-             <div className='flex justify-center h-auto mt-2  '>
-             <NavLink style={{ textDecoration: "none" }} to={"/cartbuywashingmachine" }>
-                                 <button    onClick={()=>{
-                           setCartItems([item]);
-                          }} className=' my-1 mx-8 px-10 py-1 text-md font-sans bg-gradient-to-r from-yellow-500 via-gray-500 to-yellow-500 
-                                        rounded-tr-md rounded-bl-md rounded-tl-2xl rounded-br-2xl  text-white
-                                         hover:bg-gradient-to-r hover:from-gray-500
-                                        hover:via-yellow-800 hover:to-gray-500'
-                                  > 
-                                 خرید</button></NavLink>
-                                 </div>
-             </div>
-                         </div>
-                    </div>
-                  </div>
+               <Cards curr={item}/>
              </NavLink>
                    
                  ))}
